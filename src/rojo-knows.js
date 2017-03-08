663721459fc033e654ab0073714b6536aaa3c287
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-03-08 15:14:07 -0800

    changed RojoKnows from class to object literal

--- src/rojo-knows.js
+++ src/rojo-knows.js
@@ -1,39 +1,14 @@
-class RojoKnows {
-  constructor() {
-    this.knows = {}
-
-    this.populateNames();
-    this.populateLocations();
-    this.populateActions();
-    this.populateActivities();
-    this.populateDescriptions();
-  }
-
-  populateNames() {
-    this.knows.names = ["Ringo", "Jon", "Justin", "DJ", "Scott"];
-  }
-
-  populateLocations() {
-    this.knows.locations = ["red", "home", "McDonalds"];
-  }
-
-  populateActions() {
-    this.knows.actions = ["run", "study", "slap"];
-  }
-
-  populateActivities() {
-    this.knows.activities = {
-      "sports": {
-        "hockey": "fun",
-        "basketball": "sucks",
-        "soccer": "really sucks"
-      }
+const RojoKnows = {
+  names: ["Ringo", "Jon", "Justin", "DJ", "Scott"],
+  locations: ["red", "home", "McDonalds"],
+  actions: ["run", "study", "slap"],
+  activities: {
+    sports: {
+      hockey: "fun",
+      basketball: "kinda fun",
+      soccer: "not fun"
     }
-  }
-
-  addKnowledge(category, knowledge){
-    this.knows[category] = knowledge;
   }
 }
 
-module.exports = RojoKnows;
+module.exports = RojoKnows
