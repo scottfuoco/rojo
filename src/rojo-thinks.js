4d40e2dca52c17efaed84ea5b961aaccc29331a8
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2017-03-08 15:16:02 -0800

    removed unnecessary || null checks

--- src/rojo-thinks.js
+++ src/rojo-thinks.js
@@ -2,21 +2,21 @@ const rojoKnows = require('./rojo-knows');
 
 class RojoThinks extends rojoKnows {
   constructor(){
-    this.think = {};
+    this.thinks = {};
   }
 
   think(input) {
-    this.think.questionType = questionType(input);
+    this.thinks.questionType = questionType(input);
   }
 
   questionType(input) {
     const re = /(who|what|when|where|why)/i;
-    return input.match(re)[0] || null;
+    return input.match(re)[0];
   }
 
   dayOrTime(input) {
     const re = /(day|time)/i;
-    return input.match(re)[0] || null;
+    return input.match(re)[0];
   }
 
 }
